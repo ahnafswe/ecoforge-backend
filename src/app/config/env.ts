0da77f69612de11c0e30c1e37067cfbd9c886f4c
mahnafdev <mahnafdev@gmail.com>
2026-04-18 15:31:43 +0600

    feat(utils): create token utilities

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -9,6 +9,12 @@ interface EnvConfig {
 	BETTER_AUTH_URL: string;
 	GOOGLE_CLIENT_ID: string;
 	GOOGLE_CLIENT_SECRET: string;
+	ACCESS_TOKEN_SECRET: string;
+	REFRESH_TOKEN_SECRET: string;
+	ACCESS_TOKEN_EXPIRES_IN: string;
+	REFRESH_TOKEN_EXPIRES_IN: string;
+	SESSION_TOKEN_EXPIRES_IN: string;
+	SESSION_TOKEN_UPDATE_AGE: string;
 	DATABASE_URL: string;
 }
 
@@ -20,6 +26,12 @@ const loadEnvVars = (): EnvConfig => {
 		"BETTER_AUTH_URL",
 		"GOOGLE_CLIENT_ID",
 		"GOOGLE_CLIENT_SECRET",
+		"ACCESS_TOKEN_SECRET",
+		"REFRESH_TOKEN_SECRET",
+		"ACCESS_TOKEN_EXPIRES_IN",
+		"REFRESH_TOKEN_EXPIRES_IN",
+		"SESSION_TOKEN_EXPIRES_IN",
+		"SESSION_TOKEN_UPDATE_AGE",
 		"DATABASE_URL",
 	];
 	requiredEnvVars.forEach((envVar) => {
@@ -36,6 +48,12 @@ const loadEnvVars = (): EnvConfig => {
 		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
 		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
 		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+		ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+		REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
+		ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+		REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
+		SESSION_TOKEN_EXPIRES_IN: process.env.SESSION_TOKEN_EXPIRES_IN as string,
+		SESSION_TOKEN_UPDATE_AGE: process.env.SESSION_TOKEN_UPDATE_AGE as string,
 		DATABASE_URL: process.env.DATABASE_URL as string,
 	};
 };
